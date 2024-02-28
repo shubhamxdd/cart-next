9a76082f1d13ff51c7e12f8f2e130a36e0ce0d44
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-02-28 09:33:41 +0530

    some checks

--- app/cart/checkout/CheckoutTable.tsx
+++ app/cart/checkout/CheckoutTable.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -18,44 +19,60 @@ const CheckoutTable = () => {
   const cartTotal = cart.reduce((acc, item) => acc + item.price, 0);
   return (
     <>
-      <Table>
-        <TableCaption>Your Cart</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Image</TableHead>
-            <TableHead className="px-5">Title</TableHead>
-            <TableHead>Rating</TableHead>
-            <TableHead className="text-right">Price</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {cart.map((item) => {
-            return (
-              <TableRow key={item.id}>
-                <TableCell>
-                  <Image
-                    src={item.image}
-                    alt={item.title}
-                    width={50}
-                    height={50}
-                  />
-                </TableCell>
-                <TableCell className="px-5">{item.title}</TableCell>
-                <TableCell>{item.rating.rate}⭐</TableCell>
-                <TableCell className="text-right">${item.price}</TableCell>
+      {cart.length === 0 ? (
+        <div className="text-center mt-5">
+          <p>Your cart is empty</p>
+        </div>
+      ) : (
+        <>
+          <Table>
+            <TableCaption>Your Cart</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Image</TableHead>
+                <TableHead className="px-5">Title</TableHead>
+                <TableHead>Rating</TableHead>
+                <TableHead className="text-right">Price</TableHead>
               </TableRow>
-            );
-          })}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell className="py-3" colSpan={3}>Total</TableCell>
-            <TableCell className="text-right font-semibold">
-              ${cartTotal.toFixed(2)}
-            </TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
+            </TableHeader>
+            <TableBody>
+              {cart.map((item) => {
+                return (
+                  <TableRow key={item.id}>
+                    <TableCell>
+                      <Image
+                        src={item.image}
+                        alt={item.title}
+                        width={50}
+                        height={50}
+                      />
+                    </TableCell>
+                    <TableCell className="px-5">{item.title}</TableCell>
+                    <TableCell>{item.rating.rate}⭐</TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell className="py-3" colSpan={3}>
+                  Total
+                </TableCell>
+                <TableCell className="text-right font-semibold">
+                  ${cartTotal.toFixed(2)}
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+          <div className="flex justify-between my-2">
+            <Button className="" variant={"ghost"}>
+              Go Back
+            </Button>
+            <Button className="">Make Payment</Button>
+          </div>
+        </>
+      )}
     </>
   );
 };
