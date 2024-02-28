380c2b75e395c0e3b9fda5615fc8eaf7375d0602
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-02-28 22:49:22 +0530

    remove  unused imports

--- app/signup/loading.tsx
+++ app/signup/loading.tsx
@@ -1,8 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Skeleton } from "@/components/ui/skeleton";
-import Link from "next/link";
 
 const SignupLoading = () => {
   return (
