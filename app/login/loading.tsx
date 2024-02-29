f37bc97069a4d6a63fb97b510eb02ae4f97c6346
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-02-29 22:11:17 +0530

    todo

--- app/login/loading.tsx
+++ app/login/loading.tsx
@@ -3,23 +3,26 @@ import { Skeleton } from "@/components/ui/skeleton";
 const LoginLoading = () => {
   return (
     <>
-      <Skeleton className="w-[30%] mb-6 h-10 rounded-lg" />
+    {/* todo signup ka bhi center  */}
+      <div className="flex flex-col justify-center h-full">
+        <Skeleton className="w-[30%] mb-6 h-10 rounded-lg" />
 
-      <div className="border-[1px] border-zinc-400 px-10 py-5 rounded-lg shadow-lg max-w-md mx-auto dark:bg-gray-800 dark:border-gray-700">
-        <form>
-          <div className="mb-4">
-            <Skeleton className="w-full h-4 mb-2 rounded" />
+        <div className="border-[1px] border-zinc-400 px-10 py-5 rounded-lg shadow-lg max-w-md mx-auto dark:bg-gray-800 dark:border-gray-700">
+          <form>
+            <div className="mb-4">
+              <Skeleton className="w-full h-4 mb-2 rounded" />
+              <Skeleton className="w-full h-10 rounded-md" />
+            </div>
+            <div className="mb-6">
+              <Skeleton className="w-full h-4 mb-2 rounded" />
+              <Skeleton className="w-full h-10 rounded-md" />
+            </div>
+            <div className="mb-4 -mt-2">
+              <Skeleton className="w-full h-4 rounded" />
+            </div>
             <Skeleton className="w-full h-10 rounded-md" />
-          </div>
-          <div className="mb-6">
-            <Skeleton className="w-full h-4 mb-2 rounded" />
-            <Skeleton className="w-full h-10 rounded-md" />
-          </div>
-          <div className="mb-4 -mt-2">
-            <Skeleton className="w-full h-4 rounded" />
-          </div>
-          <Skeleton className="w-full h-10 rounded-md" />
-        </form>
+          </form>
+        </div>
       </div>
     </>
   );
